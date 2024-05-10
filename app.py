a0d8ee23d5242baa766adf666a35ed7c86078f95
Ayotomiwa <ayofesnglimited@gmail.com>
2024-05-10 12:07:41 +0100

    new commit

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ import pickle
 
 app = Flask(__name__)
 
+with open('housing_price_pred_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        pass
+    else:
+        return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
